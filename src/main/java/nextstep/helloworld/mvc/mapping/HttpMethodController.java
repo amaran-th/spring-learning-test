048be631f73f94ac19a4074ad422d1fb9f17b4db
BackFoxx <backfoxxx@gmail.com>
2023-04-12 22:04:39 +0900

    feat: HttpMethodController 구현

--- src/main/java/nextstep/helloworld/mvc/mapping/HttpMethodController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/HttpMethodController.java
@@ -8,14 +8,17 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
+@RequestMapping("/http-method")
 @RestController
 public class HttpMethodController {
 
+    @PostMapping("/users")
     public ResponseEntity createUser(@RequestBody User user) {
         Long id = 1L;
         return ResponseEntity.created(URI.create("/users/" + id)).build();
     }
 
+    @GetMapping("/users")
     public ResponseEntity<List<User>> showUser() {
         List<User> users = Arrays.asList(
                 new User("이름", "email"),
